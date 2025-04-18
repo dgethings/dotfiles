d848ef43b7ed737d911bbf3e1f5209b2fc3399fd
David Gethings <dgethings@me.com>
2025-04-18 16:26:27 +0100

    NVIM: add

---
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -0,0 +1,27 @@
+-- Prefer ruff-lsp to Pyright for linting and analysis
+local lspconfig = require("lspconfig")
+local on_attach = function(client, bufnr)
+  -- Disable hover in favor of Pyright
+  client.server_capabilities.hoverProvider = false
+end
+lspconfig.ruff_lsp.setup({
+  on_attach = on_attach,
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    },
+  },
+})
+lspconfig.pyright.setup({
+  settings = {
+    pyright = {
+      disableOrganizeImports = true, -- Using Ruff
+    },
+    python = {
+      analysis = {
+        ignore = { "*" }, -- Using Ruff
+      },
+    },
+  },
+})
---
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -0,0 +1,4 @@
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_normal_opacity = 0.8
+end
