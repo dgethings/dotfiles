efc3acd345c2e5922cc162a66584e65989e40839
David Gethings <dgethings@me.com>
2025-05-20 15:14:02 +0100

    fix(nvim): apply mason v2 workaround
    Keep this until LazyVim is updated to use Mason v2
    https://github.com/LazyVim/LazyVim/pull/6053

---
+++ nvim/.config/nvim/lua/plugins/mason-v2.lua
@@ -0,0 +1,8 @@
+-- this is a workaround until Mason v2 official support in LazyVim
+return {
+  {
+    "LazyVim/LazyVim",
+    url = "https://github.com/dpetka2001/LazyVim",
+    branch = "fix/mason-v2",
+  },
+}
