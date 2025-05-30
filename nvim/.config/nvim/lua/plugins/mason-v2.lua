895eaf0f46e37f61cfad4920dcfd64158d2fb9ce
David Gethings <dgethings@me.com>
2025-05-30 19:22:39 +0100

    fix(neovim): remove mason v2 workaround

--- nvim/.config/nvim/lua/plugins/mason-v2.lua
+++
@@ -1,8 +0,0 @@
--- this is a workaround until Mason v2 official support in LazyVim
-return {
-  {
-    "LazyVim/LazyVim",
-    url = "https://github.com/dpetka2001/LazyVim",
-    branch = "fix/mason-v2",
-  },
-}
