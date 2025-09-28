9767be4abe7107f203acb2cbe2c9e357750c6506
David Gethings <dgethings@me.com>
2025-09-28 22:29:42 +0100

    fix(nvim): tryo to aviod pyright and ruff clashing

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,27 +1,34 @@
 -- Prefer ruff-lsp to Pyright for linting and analysis
-local lspconfig = require("lspconfig")
-local on_attach = function(client, bufnr)
-  -- Disable hover in favor of Pyright
-  client.server_capabilities.hoverProvider = false
-end
-lspconfig.ruff_lsp.setup({
-  on_attach = on_attach,
+-- local lspconfig = require("lspconfig")
+-- local on_attach = function(client, bufnr)
+--   -- Disable hover in favor of Pyright
+--   client.server_capabilities.hoverProvider = false
+-- end
+-- lspconfig.ruff_lsp.setup({
+--   on_attach = on_attach,
+--   init_options = {
+--     settings = {
+--       -- Any extra CLI arguments for `ruff` go here.
+--       args = {},
+--     },
+--   },
+-- })
+-- lspconfig.pyright.setup({
+--   settings = {
+--     pyright = {
+--       disableOrganizeImports = true, -- Using Ruff
+--     },
+--     python = {
+--       analysis = {
+--         ignore = { "*" }, -- Using Ruff
+--       },
+--     },
+--   },
+-- })
+require("lspconfig").ruff.setup({
   init_options = {
     settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    },
-  },
-})
-lspconfig.pyright.setup({
-  settings = {
-    pyright = {
-      disableOrganizeImports = true, -- Using Ruff
-    },
-    python = {
-      analysis = {
-        ignore = { "*" }, -- Using Ruff
-      },
+      -- Ruff language server settings go here
     },
   },
 })
