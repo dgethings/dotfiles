9f658b9d02cfc3230019ccd7aa4a8758f44f68bc
David Gethings <dgethings@me.com>
2026-06-14 22:28:17 +0400

    fix(neovim): ensure sensible langs installed for treesiter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,3 +1,20 @@
 return {
   { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    ensure_installed = {
+      "bash",
+      "go",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "vim",
+      "yaml",
+    },
+  },
 }
